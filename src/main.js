ac9caa3eee8459ddce635ba3572c9d6368c5bb35
luoye2018ly <luoye2018ly@gmail.com>
2023-10-11 11:06:11 +0800

    modified:   src/App.vue 	new file:   src/assets/css/global.css 	new file:   src/components/Home.vue 	new file:   src/components/Login.vue 	modified:   src/main.js 	new file:   src/router.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import router from './router';
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios';
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$http = axios
+// 直接全部引入而非按需引入时，默认会为我们在原型对象中添加$message，并不需要单独添加
+// Vue.prototype.$message = ElementUI.Message
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
