b0831175ef9f59ecc1117b123ffeb3ae565e2428
luoye <luoye2018ly@gmail.com>
2023-10-15 00:51:36 +0800

    Change

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -29,12 +29,21 @@
         <el-table-column label="电话" prop="mobile"></el-table-column>
         <el-table-column label="角色" prop="role_name"></el-table-column>
         <el-table-column label="状态">
+        <!--
+          此处能接到scope是因为在el-table创建的时候通过v-bind将this.userList绑定给了el-table的data
           <template v-slot="scope">
             <el-switch v-model="scope.row.mg_state"></el-switch>
           </template>
+        -->
+          <!-- 做了二次解构赋值，先从对象中将row解构出来，再从row中将mg_state解构出来 -->
+          <template v-slot="{row:{mg_state}}">
+            <el-switch v-model="mg_state"></el-switch>
+          </template>
         </el-table-column>
         <el-table-column label="操作">
+          <template v-slot="scope">
 
+          </template>
         </el-table-column>
       </el-table>
     </el-card>
