3fe5ca49d60bd623446778b135d0d2cd2119b26e
luoye <luoye2018ly@gmail.com>
2023-10-15 12:54:09 +0800

    Change

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -29,23 +29,43 @@
         <el-table-column label="电话" prop="mobile"></el-table-column>
         <el-table-column label="角色" prop="role_name"></el-table-column>
         <el-table-column label="状态">
-        <!--
-          此处能接到scope是因为在el-table创建的时候通过v-bind将this.userList绑定给了el-table的data
-          <template v-slot="scope">
-            <el-switch v-model="scope.row.mg_state"></el-switch>
-          </template>
-        -->
+          <!--
+            此处能接到scope是因为在el-table创建的时候通过v-bind将this.userList绑定给了el-table的data
+            <template v-slot="scope">
+              <el-switch v-model="scope.row.mg_state"></el-switch>
+            </template>
+          -->
           <!-- 做了二次解构赋值，先从对象中将row解构出来，再从row中将mg_state解构出来 -->
           <template v-slot="{row:{mg_state}}">
             <el-switch v-model="mg_state"></el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="180px">
           <template v-slot="scope">
-
+            <!-- 修改按钮 -->
+            <el-tooltip effect="dark" content="修改角色" placement="top" :enterable="false">
+              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            </el-tooltip>
+            <!-- 删除按钮 -->
+            <el-tooltip effect="dark" content="删除角色" placement="top" :enterable="false">
+              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            </el-tooltip>
+            <!-- 分配角色按钮 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
           </template>
         </el-table-column>
       </el-table>
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-sizes="[1, 2, 5, 10]"
+          :page-size="queryInfo.pagesize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -57,7 +77,9 @@ export default {
       // 获取用户数据列表的参数对象
       queryInfo: {
         query: '',
+        // 当前页数
         pagenum: 1,
+        // 当前每页显示数据条数
         pagesize: 2
       },
       userList: [],
@@ -75,6 +97,14 @@ export default {
       }
       this.userList = result.data.users
       this.total = result.data.total
+    },
+    // 监听 pageSize 改变的事件
+    handleSizeChange(newSize){
+      this.queryInfo.pagesize = newSize
+    },
+    // 监听 页码值 改变的事件
+    handleCurrentChange(newPage){
+      console.log(newPage)
     }
   }
 }
