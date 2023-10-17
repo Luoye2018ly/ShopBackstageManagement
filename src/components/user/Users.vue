545a4c2952fff5329b9c24076411bd819221d901
luoye2018ly <luoye2018ly@gmail.com>
2023-10-17 18:53:55 +0800

    Change

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -74,26 +74,31 @@
           @current-change="handleCurrentChange">
       </el-pagination>
     </el-card>
+    <!-- 添加用户对话框 -->
     <el-dialog
         :visible.sync="addDialogVisible"
         title="Add Users"
-        width="30%">
+        width="30%"
+        @close="addDialogClosed">
       <!-- 内容主体区 -->
       <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="70px">
         <el-form-item label="用户名" prop="username">
           <el-input v-model="addForm.username"></el-input>
-        </el-form-item><el-form-item label="密码" prop="password">
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
           <el-input v-model="addForm.password"></el-input>
-        </el-form-item><el-form-item label="邮箱" prop="email">
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
           <el-input v-model="addForm.email"></el-input>
-        </el-form-item><el-form-item label="手机" prop="mobile">
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
           <el-input v-model="addForm.mobile"></el-input>
         </el-form-item>
       </el-form>
       <!-- 底部区域 -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -102,6 +107,24 @@
 <script>
 export default {
   data() {
+    // 验证邮箱规则
+    let checkEmail = (rules,value,callback) => {
+      // 验证邮箱正则
+      const regEmail = /^([a-zA-z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)){
+        return callback()
+      }
+      callback(new Error("Please enter correct email"))
+    }
+    // 验证手机号规则
+    let checkMobile = (rules,value,callback) => {
+      // 验证手机号正则
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[0-9]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return callback()
+      }
+      return callback(new Error('Please enter correct phone number'))
+    }
     return {
       // 获取用户数据列表的参数对象
       queryInfo: {
@@ -116,19 +139,21 @@ export default {
       // 控制添加用户对话框的显示或隐藏
       addDialogVisible: false,
       addForm: {
-        username:"",
-        password:"",
-        email:"",
-        mobile:""
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
       },
       // 添加表单的验证规则对象
       addFormRules: {
         username: [{required: true, message: "Please enter username", trigger: "blur"},
-                   {min: 3, max: 10, message: "Please limit the length from 3 to 10 characters", trigger: "blur"}],
+          {min: 3, max: 10, message: "Please limit the length from 3 to 10 characters", trigger: "blur"}],
         password: [{required: true, message: "Please enter password", trigger: "blur"},
-                   {min: 6, max: 15, message: "Please limit the length from 6 to 15 characters", trigger: "blur"}],
-        email: [{required: true, message:"Please enter email"}],
-        mobile:[{required:true,message:"Please enter mobile phone number"}]
+          {min: 6, max: 15, message: "Please limit the length from 6 to 15 characters", trigger: "blur"}],
+        email: [{required: true, message: "Please enter email", trigger: "blur"},
+                {validator: checkEmail,trigger: "blur"}],
+        mobile: [{required: true, message: "Please enter mobile phone number", trigger: "blur"},
+                 {validator: checkMobile,trigger: "blur"}]
       }
 
     }
@@ -155,7 +180,7 @@ export default {
       this.queryInfo.pagenum = newPage
       this.getUserList()
     },
-    // 监听switch开关状态的改变
+    // 监听 switch 开关状态的改变
     async userStateChanged(userInfo) {
       const {data: result} = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
       if (result.meta.status !== 200) {
@@ -163,6 +188,27 @@ export default {
         return this.$message.error('Failed to change userStatus!')
       }
       this.$message.success('Change userStatus successfully！')
+    },
+    // 监听添加用户对话框关闭的事件
+    addDialogClosed(){
+      this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮添加新用户预校验
+    addUser(){
+      this.$refs.addFormRef.validate(async valid=>{
+        // 校验失败，直接返回
+        if (!valid) return
+        // 校验成功，提交数据
+        const {data : result } = await this.$http.post('users',this.addForm)
+        if (result.meta.status !== 201){
+          this.$message.error('Failed to add user')
+        }
+        this.$message.success('Add user successfully')
+        // 隐藏添加用户对话框
+        this.addDialogVisible = false
+        // 重新获取用户列表
+        await this.getUserList()
+      })
     }
   }
 }
