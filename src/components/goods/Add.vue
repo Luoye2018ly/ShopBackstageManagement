7d36570e2a9b0b2b82acd6eace61f0d682f6e0d9
luoye <luoye2018ly@gmail.com>
2023-10-29 18:00:11 +0800

    Change

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -26,7 +26,7 @@
         <el-step title="完成"></el-step>
       </el-steps>
       <!-- tab栏区域 -->
-      <el-form ref="ruleForm" :model="addForm" :rules="addFormRules" label-position="top" label-width="100px">
+      <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-position="top" label-width="100px">
         <el-tabs v-model="activeIndex" :before-leave="beforeTabLeave" :tab-position="'left'" @tab-click="tabClicked">
           <el-tab-pane label="基本信息" name="0">
             <el-form-item label="商品名称" prop="goods_name">
@@ -69,23 +69,39 @@
             <!-- on-preview 用于指定图片预览事件 -->
             <!-- on-remove 用于指定图片从上传列表删除事件 -->
             <!-- list-type 用于指定图片呈现的预览方式 -->
+            <!-- on-success 用于指定图片上传成功执行的钩子 -->
             <el-upload
                 :action="uploadURL"
+                :headers="headerObj"
                 :on-preview="handlePreview"
                 :on-remove="handleRemove"
-                list-type="picture"
-                :headers="headerObj">
+                :on-success="handleSuccess"
+                list-type="picture">
               <el-button size="small" type="primary">点击上传</el-button>
             </el-upload>
           </el-tab-pane>
-          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">
+            <!-- 富文本编辑器组件 -->
+            <quill-editor v-model="addForm.goods_introduce"></quill-editor>
+            <el-button class="btnAdd" type="primary" @click="add">添加商品</el-button>
+          </el-tab-pane>
         </el-tabs>
       </el-form>
     </el-card>
+    <!-- 图片预览窗口 -->
+    <el-dialog
+        :visible.sync="previewVisible"
+        title="图片预览"
+        width="50%"
+    >
+      <img :src="previewPath" alt="" class="previewImg">
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import {cloneDeep} from "lodash"
+
 export default {
   data() {
     return {
@@ -97,7 +113,12 @@ export default {
         goods_weight: 0,
         goods_number: 0,
         // 商品所属的分类数组
-        goods_cat: []
+        goods_cat: [],
+        // 图片的数组
+        pics: [],
+        // 商品详情描述
+        goods_introduce: "",
+        attrs: []
       },
       addFormRules: {
         goods_name: [{required: true, message: "Please enter goods name", trigger: "blur"}],
@@ -117,7 +138,9 @@ export default {
       onlyTableData: {},
       uploadURL: 'http://127.0.0.1:8888/api/private/v1/upload',
       // 图片上传组件的header请求头对象
-      headerObj:{Authorization:window.sessionStorage.getItem('token')}
+      headerObj: {Authorization: window.sessionStorage.getItem('token')},
+      previewPath: "",
+      previewVisible: false
     }
   },
   created() {
@@ -162,14 +185,55 @@ export default {
       }
     },
     // 处理图片预览效果
-    handlePreview() {
-
+    handlePreview(file) {
+      this.previewPath = file.response.data.url
+      this.previewVisible = true
     },
     // 处理移除图片操作
-    handleRemove() {
-
+    handleRemove(file) {
+      // TODO 1.获取将要删除图片的临时路径
+      const filePath = file.response.data.tmp_path
+      // TODO 2.从 pics 数组中找到图片对应索引值
+      const index = this.addForm.pics.findIndex(item => item.pic === filePath)
+      // TODO 3.调用生素组的 splice 方法，把图片信息对象从 pics 数组中移除
+      this.addForm.pics.splice(index, 1)
+      console.log(this.addForm)
     },
-
+    // 监听图片上传成功的事件
+    handleSuccess(response) {
+      // TODO 1.拼接得到图片信息对象
+      // TODO 2.将图片信息 push到pics数组中
+      const picInfo = {pic: response.data.tmp_path}
+      this.addForm.pics.push(picInfo)
+    },
+    add() {
+      this.$refs.addFormRef.validate(async (valid) => {
+        if (!valid) return this.$message.error("Please fill in the necessary form fields")
+        // TODO 执行添加的业务逻辑
+        const form = cloneDeep(this.addForm)
+        form.goods_cat = form.goods_cat.join(",")
+        // TODO 处理动态参数、静态属性
+        this.manyTableData.forEach(item => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals.join(" ")
+          }
+          this.addForm.attrs.push(newInfo)
+        })
+        this.onlyTableData.forEach(item => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals
+          }
+          this.addForm.attrs.push(newInfo)
+        })
+        form.attrs = this.addForm.attrs
+        // 发起请求添加商品
+        const {data: result} = await this.$http.post("goods",form)
+        if (result.meta.status !== 201) return this.$message.error("Failed to submit goods")
+        await this.$router.push("/goods")
+      })
+    }
   },
   computed: {
     cateId() {
@@ -186,5 +250,13 @@ export default {
 <style scoped>
 .el-checkbox {
   margin: 0 15px 0 0 !important;
+}
+
+.previewImg {
+  width: 100%;
+}
+
+.btnAdd {
+  margin-top: 15px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,32 +7,39 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios';
 import TreeTable from "vue-table-with-tree-grid"
+// 导入富文本编辑器
+import VueQuillEditor from "vue-quill-editor";
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-Vue.filter("dateFormat",function (originValue){
-  const date = new Date(originValue)
-  const year = date.getFullYear()
-  const month = (date.getMonth() + 1 + "").padStart(2,"0")
-  const day = (date.getDay() + "").padStart(2,"0")
-  const hours = (date.getHours() + "").padStart(2,"0")
-  const minute = (date.getMinutes() + "").padStart(2,"0")
-  const second = (date.getSeconds() + "").padStart(2,"0")
-  return `${year}-${month}-${day} ${hours}:${minute}:${second}`
+Vue.use(VueQuillEditor)
+Vue.filter("dateFormat", function (originValue) {
+    const date = new Date(originValue)
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1 + "").padStart(2, "0")
+    const day = (date.getDay() + "").padStart(2, "0")
+    const hours = (date.getHours() + "").padStart(2, "0")
+    const minute = (date.getMinutes() + "").padStart(2, "0")
+    const second = (date.getSeconds() + "").padStart(2, "0")
+    return `${year}-${month}-${day} ${hours}:${minute}:${second}`
 })
-Vue.component("tree-table",TreeTable)
-axios.interceptors.request.use(config =>{
-  config.headers.Authorization = window.sessionStorage.getItem('token')
-  return config
+Vue.component("tree-table", TreeTable)
+axios.interceptors.request.use(config => {
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
 })
 Vue.prototype.$http = axios
 // 全局引入的ElementUI，默认会为我们在原型对象中添加$message，并不需要单独添加
 // Vue.prototype.$message = ElementUI.Message
 // 全局引入的ElementUI，与$message一样，引入时已经配置好$confirm，也不需要单独添加
-Vue.prototype.$confirm = ElementUI.MessageBox.confirm
+// Vue.prototype.$confirm = ElementUI.MessageBox.confirm
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
